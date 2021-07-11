8127b725f61c065949ace65ba157ff561f0b8493
kaizzzoku <z.zzzoku.z@gmail.com>
2021-07-11 22:36:40 +0300

    Add error logging

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require './vendor/autoload.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 
 (new \App\Env('.env'))->load();
 
