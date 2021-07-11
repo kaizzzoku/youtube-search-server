72c3b462daf2ce325e2230e00d24ee6763650f3c
kaizzzoku <z.zzzoku.z@gmail.com>
2021-07-11 22:41:51 +0300

    Check if env file needed for heroku deploy

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ require './vendor/autoload.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
+
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-(new \App\Env('.env'))->load();
+if (!getenv('WITHOUT_ENV_FILE')) {
+    (new \App\Env('.env'))->load();
+}
 
 $core = new App\Core();
 
